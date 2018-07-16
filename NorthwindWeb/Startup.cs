dcf41f18126527899a62dfef1d4e575b8819cf52
Shmulik, Shahar <ss250819@ncr.com>
2018-07-16 19:00:37 +0300

    test

--- NorthwindWeb/Startup.cs
+++ NorthwindWeb/Startup.cs
@@ -15,6 +15,7 @@ namespace NorthwindWeb
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -29,7 +30,9 @@ namespace NorthwindWeb
             //{
             //    await context.Response.WriteAsync("Hello World! ");
             //});
+            app.UseDefaultFiles(); // index.html, default.html, and so on
             app.UseStaticFiles();
+            app.UseMvc(); // includes Razor Pages
         }
     }
 }
